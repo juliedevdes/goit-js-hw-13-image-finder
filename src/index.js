2a6ca60079024212e22864271c2d6fad0cffa75b
Julia Ohrimenko <juliajam2497@gmail.com>
2021-08-22 12:29:33 +0300

    added comments to clarify

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import galleryTemplate from './handlebars/gallery.hbs';
 import refs from './js/references';
 import fetchImgs from './js/apiService';
 
-function onSubmit(event) {
+//fetchImg returns array of images related to it's argument/ we need to put it into object for handlebars and create markup..
+function apiResponseArrayHandler(imgs) {
+  const obj = { imgs: imgs.hits };
+  const markup = galleryTemplate(obj);
+  refs.container.innerHTML = markup; //...and then put it into the HTML container
+}
+
+//all fetchImg stuff should work as a response for submit btn click, so we use it here
+function onSubmitBtnClick(event) {
   event.preventDefault();
   fetchImgs(refs.input.value).then(apiResponseArrayHandler);
 }
 
-function apiResponseArrayHandler(imgs) {
-  const obj = { imgs: imgs.hits };
-  const markup = galleryTemplate(obj);
-  refs.container.innerHTML = markup;
-}
-
-refs.button.addEventListener('click', onSubmit);
+//and adding event listener, eventually
+refs.button.addEventListener('click', onSubmitBtnClick);
